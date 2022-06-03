42cdb753e0aaf4b2eaed6f5006f18ea210d74bb6
Milan Jogani <milanjogani@gmail.com>
2022-06-03 12:34:32 -0400

    first commit

---
+++ hello.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class hello {
+
+ public static void main(String[] args) {
+	 // TODO Auto-generated method stub
+	 System.out.println("This is my first programm");
+	 System.out.println("I'm enjoying java \nIt's really good");
+	 System.out.println(3 + 3);
+
+	 //Boolean data type
+	 boolean isJavaFun = true;
+	 boolean isFishTasty = false;
+	 System.out.println(isJavaFun);
+	 System.out.println(isFishTasty);
+
+	 //Widening and narrowing casting
+	 double myVal = 10.15d;
+	 int myInt = (int)myVal;
+	 System.out.println(myInt);
+	 System.out.println(myVal);
+
+
+ }
+
+}
